05a854d3511165924e5bf0243d2824a4a55f48e9
Abhi Raj <raj12oct2001@gmail.com>
2025-07-20 23:46:52 +0530

    updating bank class 2

--- MainATM.java
+++ MainATM.java
@@ -9,12 +9,12 @@ class Bank
     }
     public double Withdraw(double amount)
     {
-        if(this.amount <= amount)
+        if(this.amount >= amount)
         {
             this.amount -= amount;
             return this.amount;
         }
-        return -1;
+        return -1.0;
     }
     public void Deposit(double amount)
     {
@@ -60,11 +60,13 @@ class User
             case 1:
                 getAmountFromUser(sc);
                 this.bal = b1.Withdraw(this.amount);
-                if(this.bal == -1){System.err.println("Insufficient Balance:");}   
+                if(this.bal == -1.0){System.err.println("Insufficient Balance:");}
+                System.err.println("Your Current Balance after Withdrawing "+this.bal);   
                 break;
             case 2:
                 getAmountFromUser(sc);
                 b1.Deposit(this.amount);
+                System.err.println("Your Current Balance after Depositing "+this.bal);
                 break;
             case 3:
                 this.bal = b1.Balance();
@@ -82,8 +84,8 @@ public class MainATM
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("CODSOFT ATM");
-        System.out.println("Enter your PIN:");
+        System.out.println("----------CODSOFT ATM----------");
+        System.out.println("        Enter your PIN:");
         pin = sc.nextInt();
         User u1 = new User();
         boolean validPin = u1.isPinCorrect(pin);
@@ -92,11 +94,11 @@ public class MainATM
             while(true)
             {
                 System.out.println("Hello, "+u1.getName());
-                System.out.println("1: Withdraw");
-                System.out.println("2: Deposit");
-                System.out.println("3: Balance");
-                System.out.println("4: Change PIN");
-                System.out.println("5: Exit");
+                System.out.println("        1: Withdraw");
+                System.out.println("        2: Deposit");
+                System.out.println("        3: Balance");
+                System.out.println("        4: Change PIN");
+                System.out.println("        5: Exit");
                 int choice = sc.nextInt();
                 if(choice!= 5){u1.connectBank(choice,sc);}
                 else if(choice == 4)
